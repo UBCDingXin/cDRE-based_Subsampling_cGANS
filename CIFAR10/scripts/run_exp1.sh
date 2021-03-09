8dc50d4e27fe320028107dd2dbd7adb58371d09a
UBCDingXin <dingx92@gmail.com>
2021-03-09 16:38:11 +0800

    edit files

--- CIFAR10/scripts/run_exp1.sh
+++ CIFAR10/scripts/run_exp1.sh
@@ -1,33 +1,32 @@
 ############################################
 # EXP 1: NTRAIN=50K, BigGAN
-ROOT_PATH="/home/xin/OneDrive/Working_directory/Subsample_cGANs_via_cDRE/CIFAR10_v2"
+ROOT_PATH="./CIFAR10"
 
 NCPU=8
 NTRAIN=50000
 EXP_NAME="main_ntrain_${NTRAIN}"
 GAN_EPOCHS=2000
 NFAKE_PER_CLASS=5000
-PreCNN="ResNet34" ##ResNet34 or DenseNet121
+PreCNN="ResNet34"
 
 
-# echo "-------------------------------------------------------------------------------------------------"
-# echo "No Subsampling"
-# CUDA_VISIBLE_DEVICES=1,0 python3 main.py \
-# --exp_name $EXP_NAME --root_path $ROOT_PATH --num_workers $NCPU \
-# --ntrain $NTRAIN \
-# --gan_epochs $GAN_EPOCHS \
-# --dre_precnn_net $PreCNN \
-# --dre_precnn_epochs 350 --dre_precnn_resume_epoch 0 \
-# --dre_precnn_lr_base 0.1 --dre_precnn_lr_decay_factor 0.1 --dre_precnn_lr_decay_epochs 150_250 \
-# --dre_precnn_batch_size_train 128 --dre_precnn_weight_decay 1e-4 --dre_precnn_transform \
-# --dre_mode conditional --dre_net MLP5 \
-# --dre_epochs 350 --dre_resume_epoch 0 \
-# --dre_lr_base 1e-4 --dre_lr_decay_factor 0.1 --dre_lr_decay_epochs 150_250 \
-# --dre_batch_size 128 --dre_lambda 1e-3 \
-# --visualize_fake_images \
-# --eval --eval_FID_batch_size 200 \
-# --samp_batch_size 1000 --samp_burnin_size 10000 --samp_nfake_per_class $NFAKE_PER_CLASS \
-
+echo "-------------------------------------------------------------------------------------------------"
+echo "No Subsampling"
+CUDA_VISIBLE_DEVICES=1,0 python3 main.py \
+--exp_name $EXP_NAME --root_path $ROOT_PATH --num_workers $NCPU \
+--ntrain $NTRAIN \
+--gan_epochs $GAN_EPOCHS \
+--dre_precnn_net $PreCNN \
+--dre_precnn_epochs 350 --dre_precnn_resume_epoch 0 \
+--dre_precnn_lr_base 0.1 --dre_precnn_lr_decay_factor 0.1 --dre_precnn_lr_decay_epochs 150_250 \
+--dre_precnn_batch_size_train 128 --dre_precnn_weight_decay 1e-4 --dre_precnn_transform \
+--dre_mode conditional --dre_net MLP5 \
+--dre_epochs 350 --dre_resume_epoch 0 \
+--dre_lr_base 1e-4 --dre_lr_decay_factor 0.1 --dre_lr_decay_epochs 150_250 \
+--dre_batch_size 128 --dre_lambda 1e-3 \
+--visualize_fake_images \
+--eval --eval_FID_batch_size 200 \
+--samp_batch_size 1000 --samp_burnin_size 10000 --samp_nfake_per_class $NFAKE_PER_CLASS \
 
 
 echo "-------------------------------------------------------------------------------------------------"
@@ -50,22 +49,22 @@ CUDA_VISIBLE_DEVICES=1,0 python3 main.py \
 --samp_batch_size 1000 --samp_burnin_size 10000 --samp_nfake_per_class $NFAKE_PER_CLASS \
 
 
-# dre_resume_epoch=0
-# echo "-------------------------------------------------------------------------------------------------"
-# echo "Subsampling by Conditional DRE-F-SP+RS"
-# CUDA_VISIBLE_DEVICES=1,0 python3 main.py \
-# --exp_name $EXP_NAME --root_path $ROOT_PATH --num_workers $NCPU \
-# --ntrain $NTRAIN \
-# --gan_epochs $GAN_EPOCHS \
-# --dre_precnn_net $PreCNN \
-# --dre_precnn_epochs 350 --dre_precnn_resume_epoch 0 \
-# --dre_precnn_lr_base 0.1 --dre_precnn_lr_decay_factor 0.1 --dre_precnn_lr_decay_epochs 150_250 \
-# --dre_precnn_batch_size_train 128 --dre_precnn_weight_decay 1e-4 --dre_precnn_transform \
-# --dre_mode conditional --dre_net MLP5 \
-# --dre_epochs 350 --dre_resume_epoch $dre_resume_epoch \
-# --dre_lr_base 1e-4 --dre_lr_decay_factor 0.1 --dre_lr_decay_epochs 150_250 \
-# --dre_batch_size 128 --dre_lambda 1e-3 \
-# --subsampling \
-# --visualize_fake_images \
-# --eval --eval_FID_batch_size 200 \
-# --samp_batch_size 1000 --samp_burnin_size 10000 --samp_nfake_per_class $NFAKE_PER_CLASS \
+dre_resume_epoch=0
+echo "-------------------------------------------------------------------------------------------------"
+echo "Subsampling by Conditional DRE-F-SP+RS"
+CUDA_VISIBLE_DEVICES=1,0 python3 main.py \
+--exp_name $EXP_NAME --root_path $ROOT_PATH --num_workers $NCPU \
+--ntrain $NTRAIN \
+--gan_epochs $GAN_EPOCHS \
+--dre_precnn_net $PreCNN \
+--dre_precnn_epochs 350 --dre_precnn_resume_epoch 0 \
+--dre_precnn_lr_base 0.1 --dre_precnn_lr_decay_factor 0.1 --dre_precnn_lr_decay_epochs 150_250 \
+--dre_precnn_batch_size_train 128 --dre_precnn_weight_decay 1e-4 --dre_precnn_transform \
+--dre_mode conditional --dre_net MLP5 \
+--dre_epochs 350 --dre_resume_epoch $dre_resume_epoch \
+--dre_lr_base 1e-4 --dre_lr_decay_factor 0.1 --dre_lr_decay_epochs 150_250 \
+--dre_batch_size 128 --dre_lambda 1e-3 \
+--subsampling \
+--visualize_fake_images \
+--eval --eval_FID_batch_size 200 \
+--samp_batch_size 1000 --samp_burnin_size 10000 --samp_nfake_per_class $NFAKE_PER_CLASS \
--- README.md
+++ README.md
@@ -4,20 +4,27 @@
 ## Datasets (h5 files)
 
 ### CIFAR-10 (50000 training samples)
-For BigGAN training: <br />
-
-
+Put the following h5 file at './CIFAR10/data': <br />
+https://1drv.ms/u/s!Arj2pETbYnWQsdtpYbSbbb7ntilamQ?e=AKaVHF <br />
 
 ### CIFAR-10 (20000 training samples)
+Put the following h5 file at './CIFAR10/data': <br />
+https://1drv.ms/u/s!Arj2pETbYnWQsdtqZzH-nTaHSqq-ug?e=svEixn <br />
 
-
+Put the following h5 file at './CIFAR10/BigGAN_20K/data': <br />
+https://1drv.ms/u/s!Arj2pETbYnWQsd5yb7DgNhrJAaWZ3A?e=5a5NtL <br />
 
 ### CIFAR-10 (10000 training samples)
+Put the following h5 file at './CIFAR10/data': <br />
+https://1drv.ms/u/s!Arj2pETbYnWQsdtr0xfBIgiKANoobA?e=bfFMnQ <br />
 
-
+Put the following h5 file at './CIFAR10/BigGAN_10K/data': <br />
+https://1drv.ms/u/s!Arj2pETbYnWQsd44tlt0C_4YLktKcQ?e=ZY9XM8 <br />
 
 ### The RC-49 Dataset
-https://1drv.ms/u/s!Arj2pETbYnWQr7MY2Pr5qipSUpZKEQ?e=QTbiq2 <br />
+Put the following h5 file at './RC-49/data': <br />
+https://1drv.ms/u/s!Arj2pETbYnWQso1vD_2hHtvbxVOM9w?e=jgK2Se <br />
+
 
 -------------------------------
 ## Sample Usage
